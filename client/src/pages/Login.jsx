b0dbbe795aedb69d90070952b338684f3903e7c3
Gabriel Soares <gabrielfsoares0706@gmail.com>
2025-11-11 23:08:48 -0300

    ajuste login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { Container } from "../styles/Login";
 
 // import Navbar from "../components/Navbar";
 // import Header from "../components/Header";
 
 const Login = () => {
   return (
-    
+    <Container>
       <div>
         <h1>LOGIN</h1>
       </div>
+    </Container>
     
   );
 };
