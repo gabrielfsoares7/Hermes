1f3fa12b1f7f5a3068c5ab0282b942a5b59c7bff
Gabriel Soares <gabrielfsoares0706@gmail.com>
2025-11-18 22:35:29 -0300

    add header

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ContainerHeader } from "./style";
+
+// import Navbar from "../components/Navbar";
+// import Header from "../components/Header";
+
+const Header = () => {
+  return (
+    <ContainerHeader>
+      <div>
+        <h1>Header</h1>
+      </div>
+    </ContainerHeader>
+  );
+};
+
+export default Header;
